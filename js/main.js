ed14bef95d70b635ee1b1cdab61b560fa4bb0fd9
Franklin <franklinfs390@gmail.com>
2020-08-01 00:09:31 -0300

    Adding Projects.

--- js/main.js
+++ js/main.js
@@ -99,6 +99,16 @@
         navigationText: false
     });
 
+	$("#owl-slider-languages").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [1500, 1],
+	        [1500, 1]
+	     ],
+        navigationText: false
+    });
 
 	/*----------------------------------------------------- */
 	/* Alert Boxes
